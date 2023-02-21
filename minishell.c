a51f399c5dcd6fad4393b17fc105e3987edecac3
AliHaine <alitr57120@gmail.com>
2023-02-21 15:44:04 +0100

    ctrl c et ctrl d

--- minishell.c
+++ minishell.c
@@ -31,12 +31,14 @@ static int	main_process(struct s_minishell *ms)
 	while (ms->exit > 0)
 	{
 		histo = readline("\U0001F1F9\U0001F1F7  >> ");
-		check_all_cmd(histo);
+		if (!histo)
+			break;
 		if (check_validity(histo))
 		{
 			add_history(histo);
 			write_to_histo((char *) histo, ms->histo_fd);
 		}
+		check_all_cmd(histo);
 	}
 	return (1);
 }
@@ -46,6 +48,7 @@ int main (int argc, char **argv, char **env)
 	struct s_minishell ms;
 
 	sigaction_manager(&ms);
+	rl_catch_signals = 0;
 	ms.histo_fd =  open(".history", O_RDWR);
 	if (ms.histo_fd == -1)
 	{
@@ -56,5 +59,6 @@ int main (int argc, char **argv, char **env)
 	ms.exit = 1;
 	go_to_end_of_file(ms.histo_fd);
 	main_process(&ms);
+	close(ms.histo_fd);
 	return (0);
 }
--- srcs/signal.c
+++ srcs/signal.c
@@ -8,7 +8,7 @@ void	ctrl_c()
 {
 	write(1, "\n", 1);
 	rl_on_new_line();
-	//rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
