6776aa71c7413ddb02ce456cc885f359f8ffccf2
Marwen <108383159+MarwenB07@users.noreply.github.com>
2023-03-11 12:55:17 +0100

    Add files via upload

--- srcs/execve.c
+++ srcs/execve.c
@@ -1,19 +1,27 @@
 #include "../minishell.h"
 
+
 int	ft_execve(char *const *args, char **env)
 {
 	char **bash;
+	pid_t pid;
 	int i;
-	int rd;
-
+	int test;
+	
+	pid = fork();
 	i = 0;
-	rd = -1;
-	bash = ft_split(ft_getenv(env, "PATH"), ':');
-	if (!bash)
-		return (-1);
-	while (rd == -1)
+	test = -1;
+	if (pid == 0) 
 	{
-		rd = execve(ft_join(bash[i++], args[0]), args, NULL);
+		bash = ft_split(ft_getenv(env, "PATH"), ':');
+		if (!bash)
+			return (-1);
+		while (test < 0)
+		{
+			test = execve(ft_join(bash[i++], args[0]), args, NULL);
+		}
 	}
+	else
+		waitpid(pid, &test, 0);
 	return (0);
 }
