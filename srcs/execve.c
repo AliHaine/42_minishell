42e70bdbfb4e1bc72b241c7ffaf0dcb1525546e6
Marwen <108383159+MarwenB07@users.noreply.github.com>
2023-03-31 15:30:21 +0200

    Add files via upload

--- srcs/execve.c
+++ srcs/execve.c
@@ -6,7 +6,7 @@
 /*   By: mbouaza <mbouaza@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 06:59:43 by mbouaza           #+#    #+#             */
-/*   Updated: 2023/03/29 11:01:10 by mbouaza          ###   ########.fr       */
+/*   Updated: 2023/03/31 02:23:41 by mbouaza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,24 @@ static void	ft_execve2(char **args)
 	free_tt(args);
 }
 
-int	ft_execve(t_cmds *cmd, char **env)
+int	ft_execve(t_cmds *cmd, char **env, int i)
 {
-	int		i;
 	char	**args;
 	char	*join;
+	char	**bash;
+	char	*path;
 
-	i = 0;
+	path = ft_getenv(env, "PATH");
+	bash = ft_split(path, ':');
+	free(path);
 	args = ft_split(cmd->cmd_args, ' ');
-	while (g_ms.bash[i])
+	while (bash[i])
 	{
-		join = ft_join(g_ms.bash[i++], cmd->cmd);
+		join = ft_join(bash[i++], cmd->cmd);
 		execve(join, args, NULL);
 		free(join);
 	}
 	ft_execve2(args);
+	free_tt(bash);
 	return (0);
 }
